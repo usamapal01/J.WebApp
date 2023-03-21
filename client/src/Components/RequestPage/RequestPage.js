0b665f20a1d90239baee1f1d37f5aa903c2a9d5e
usamapal01 <usamapal01@users.noreply.github.com>
2023-03-20 17:39:59 -0700

    minor changes to frontend

--- client/src/Components/RequestPage/RequestPage.js
+++ client/src/Components/RequestPage/RequestPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
+// import './RequestPage.css';
+
 const RequestPage = () => {
   const [requestData, setRequestData] = useState({});
 
@@ -20,11 +22,56 @@ const RequestPage = () => {
 
   return (
     <div>
-      <h2>Request {request_id}</h2>
-      <p>Phone number: {requestData.phone_number}</p>
-      <p>Request text: {requestData.request_text}</p>
-      <p>Customer name: {requestData.first_name} {requestData.last_name}</p>
+      <h1>Request {requestData.request_id}</h1>
+      <p><b>Customer ID:</b> {requestData.cust_id}</p>
+      <p><b>Customer name:</b> {requestData.first_name} {requestData.last_name}</p>
+      <p><b>Phone number:</b> {requestData.phone_number}</p>
+      <p><b>Request text:</b> {requestData.request_text}</p>
     </div>
+
+    //Code sent by Andreas
+    // <div className="page">
+    //   <div className="row">
+    //     <div className="column">
+    //       <h2 className="Customer_ID_label"> Customer ID </h2>
+    //       <h3 className="value">{requestData.cust_id} </h3>
+    //     </div>
+    //     <div className="column">
+    //       <h2 className="Request_ID_label"> Request ID </h2>
+    //       <h3 className="value">{requestData.request_id} </h3>
+    //     </div>
+    //   </div>
+    //   <div className="row">
+    //     <div className="column">
+    //       <h2 className="First_name_label"> First Name </h2>
+    //       <h3 className="value">{requestData.first_name} </h3>
+    //     </div>
+    //     <div className="column">
+    //       <h2 className="Last_name_label"> Last Name </h2>
+    //       <h3 className="value">{requestData.last_name} </h3>
+    //     </div>
+    //   </div>
+
+    //   <div className="column">
+    //     <h2 className="Phone_number_label"> Phone Number </h2>
+    //     <h3 className="value">{requestData.phone_number} </h3>
+    //   </div>
+    //   <div className="spacer" />
+
+    //   <div className="Request_data_div">
+    //     <h4>{requestData.request_text}</h4>
+    //   </div>
+
+    //   <div className="spacer" />
+
+    //   <div className="textarea_div ">
+    //     <div className="column">
+    //       <textarea className="textarea" />
+    //       <br/>
+    //       <button className="submit_button"> SUBMIT </button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
