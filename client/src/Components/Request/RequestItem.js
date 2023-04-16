5fb666f8f86735a6b1ba99019216ebc7f2900292
usamapal01 <usamapal01@users.noreply.github.com>
2023-04-16 04:00:03 -0700

    Frontend changed for phonenumber
    It will display phone number in proper format in the frontend

--- client/src/Components/Request/RequestItem.js
+++ client/src/Components/Request/RequestItem.js
@@ -19,9 +19,9 @@ const RequestItem = (props) => {
           <div className="user-item__info">
             <h1>Req Id:{props.request_id}</h1>
             <h1>Customer Id:{props.cust_id}</h1>
-            <h2>First Name: {props.first_name}</h2>
-            <h2>Last Name: {props.last_name}</h2>
-            <h2>{props.phone_number}</h2>
+            <h2>Name: {props.first_name} {props.last_name}</h2>
+            {/* <h2>Last Name: {props.last_name}</h2> */}
+            <h2>{`+1 (${props.phone_number.slice(2,5)}) ${props.phone_number.slice(5,8)}-${props.phone_number.slice(8)}`}</h2>
             <h3>
               Requests: {props.request_text}
             </h3>
