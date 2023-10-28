5ac030bb7dbb27be80109028c75eb46d47b29eaa
usamapal01 <usamapal01@users.noreply.github.com>
2023-10-28 12:18:08 -0700

    database change

--- server/database.js
+++ server/database.js
@@ -7,7 +7,7 @@ let conn = mysql.createConnection({
   host: process.env.DATABASE_HOST || "localhost",
   user: process.env.DATABASE_USER || "root",
   password: process.env.DATABASE_PASSWORD || "password",
-  // database: process.env.DATABASE || "database-1",
+  database: process.env.DATABASE || "database-1",
   connectionLimit:10
   });
 
